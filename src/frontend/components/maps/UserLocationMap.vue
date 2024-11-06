69003f9bd24d4b8029eda4e892a9245845d144da
muhammedsaidckr <muhammedsaidckr@gmail.com>
2024-11-06 17:16:26 +0300

    Added controls for update data on maps

--- src/frontend/components/maps/UserLocationMap.vue
+++ src/frontend/components/maps/UserLocationMap.vue
@@ -12,6 +12,10 @@ export default {
       loading: false,
       center: [41.3851, 36.519],
       zoom: 2,
+      isShowingControls: true,
+      autoReload: false,
+      reloadInterval: null,
+      isUpdating: false,
     };
   },
   methods: {
@@ -60,6 +64,30 @@ export default {
         console.log(e);
       }
     },
+    async update() {
+      await this.getUserLocations();
+    },
+    async onAutoReload() {
+
+      // do nothing if auto reload disabled
+      if (!this.autoReload) {
+        return;
+      }
+
+      // do nothing if already updating
+      if (this.isUpdating) {
+        return;
+      }
+
+      // auto reload
+      try {
+        this.isUpdating = true;
+        await this.update();
+      } finally {
+        this.isUpdating = false;
+      }
+
+    },
   },
   mounted() {
     this.map = new L.Map('map').fitWorld();
@@ -87,6 +115,9 @@ export default {
       L.marker(this.center).addTo(this.map);
     });
     this.getUserLocations();
+
+    // auto reload
+    this.reloadInterval = setInterval(this.onAutoReload, 5000);
   },
   beforeUnmount() {
     if (this.map) {
@@ -97,11 +128,40 @@ export default {
 </script>
 
 <template>
-  <div class="w-full h-full">
+  <div class="w-full h-full relative">
     <div id="map" class="w-full h-screen" style="min-width: 600px; min-height: 600px;"></div>
     <div v-if="loading"
          class="absolute top-0 left-0 w-full h-full bg-white bg-opacity-75 flex items-center justify-center">
       <div class="text-2xl font-bold text-gray-800"></div>
+    </div>
+    <!-- controls -->
+    <div class="absolute flex bottom-0 left-0 bg-gray-100 p-2" style="z-index: 9999">
+      <div class="bg-white rounded shadow min-w-52">
+        <div @click="isShowingControls = !isShowingControls" class="flex text-gray-700 p-2 cursor-pointer">
+          <div class="my-auto">Reserve Network</div>
+          <div class="flex ml-auto">
+            <button @click.stop="update" type="button"
+                    class="my-auto inline-flex items-center gap-x-1 rounded-md bg-gray-500 px-1 py-0.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                   stroke="currentColor" class="size-5">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div v-if="isShowingControls" class="divide-y text-gray-900 border-t border-gray-300">
+          <div class="px-1 py-2">
+            <div class="flex items-start">
+              <div class="flex items-center h-5">
+                <input v-model="autoReload" type="checkbox"
+                       class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300">
+              </div>
+              <label class="ml-2 text-sm font-medium text-gray-900">Auto Update (5 sec)</label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
